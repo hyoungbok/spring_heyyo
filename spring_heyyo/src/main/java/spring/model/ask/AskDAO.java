ce2b2d2ddb87a580a9a91edaa17db31ec7af0119
soldesk3-24 <soldesk3-24@soldesk3-24-PC>
2018-12-03 12:34:16 +0900

    변경

--- spring_heyyo/src/main/java/spring/model/ask/AskDAO.java
+++ spring_heyyo/src/main/java/spring/model/ask/AskDAO.java
@@ -3,42 +3,61 @@ package spring.model.ask;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AskDAO implements IAskDAO{
 
+	@Autowired
+	private SqlSessionTemplate mysql;
+
+	public void setMysql(SqlSessionTemplate mysql) {
+		this.mysql = mysql;
+	}	
+	
 	@Override
 	public boolean create(Object dto) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flag = false;
+		int cnt = mysql.insert("ask.create", dto);
+		if(cnt>0) flag = true;
+		return flag;
 	}
 
 	@Override
 	public List list(Map map) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return mysql.selectList("ask.list", map);
 	}
 
 	@Override
-	public Object read(Object pk) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public AskDTO read(Object ask_code) throws Exception {
+		
+		return mysql.selectOne("ask.read", ask_code);
 	}
 
 	@Override
 	public boolean update(Object dto) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flag = false;
+		int cnt = mysql.update("ask.update", dto);
+		if(cnt>0) flag = true;
+		return flag;
 	}
 
 	@Override
-	public boolean delete(Object pk) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(Object ask_code) throws Exception {
+		boolean flag = false;
+		int cnt = mysql.delete("ask.delete", ask_code);
+		if(cnt > 0) flag = true;
+		return flag;
 	}
 
 	@Override
 	public int total(Map map) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return mysql.selectOne("ask.total", map);
 	}
 
 }
--- spring_heyyo/src/main/java/spring/model/buisness_required/Buisness_requiredDAO.java
+++ spring_heyyo/src/main/java/spring/model/buisness_required/Buisness_requiredDAO.java
@@ -1,44 +1,63 @@
 package spring.model.buisness_required;
 
 import java.util.List;
+
 import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class Buisness_requiredDAO implements IBuisness_requiredDAO{
 
+	@Autowired
+	private SqlSessionTemplate mysql;
+
+	public void setMysql(SqlSessionTemplate mysql) {
+		this.mysql = mysql;
+	}	
+	
 	@Override
 	public boolean create(Object dto) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flag = false;
+		int cnt = mysql.insert("buisness_required.create",dto);
+		if(cnt>0) flag = true;
+		return flag;
 	}
 
 	@Override
 	public List list(Map map) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		return mysql.selectList("buisness_required.list", map);
 	}
 
 	@Override
-	public Object read(Object pk) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Object read(Object req_num) throws Exception {
+
+		return mysql.selectOne("buisness_required.read", req_num);
 	}
 
 	@Override
 	public boolean update(Object dto) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flag = false;
+		int cnt = mysql.update("buisness_required.update", dto);
+		if(cnt > 0) flag = true;
+		return flag;
 	}
 
 	@Override
-	public boolean delete(Object pk) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(Object req_num) throws Exception {
+		boolean flag = false;
+		int cnt = mysql.delete("buisness_required.delete", req_num);
+		if(cnt > 0) flag = true;
+		return flag;
 	}
 
 	@Override
 	public int total(Map map) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return mysql.selectOne("buisness_required.total", map);
 	}
 
 }
